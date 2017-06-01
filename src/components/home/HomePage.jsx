9d84b592b606044183d7ba3d5748b7f7d3f007b0
agirmar92 <aegir13@ru.is>
2017-06-01 21:46:35 +0000

    Adding a modal with Vimeo iframe video

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -1,18 +1,52 @@
 import React, { PureComponent } from 'react';
+var FontAwesome = require('react-fontawesome');
 
 import defaultStyles from 'global.css';
 
 import SlidingImage from 'components/common/SlidingImage';
+import VideoModal from 'components/home/VideoModal';
 
-export default class HomePage extends PureComponent {
+export default class HomePage extends React.Component {
+
+  constructor() {
+    super();
+    // By default we do not show the modal.
+    this.state = { showVideoModal: false };
+  }
+
+  toggleVideoModal = () => {
+    var oldVal = this.state.showVideoModal;
+    this.setState({ showVideoModal: !oldVal });
+  }
+
+  modalChangeHandler = (value) => {
+    this.setState({showVideoModal: value});
+  }
+
+  componentWillMount() {
+    var _this = this;
+    document.addEventListener("keydown", function(e) {
+      // If the escape button is struck
+      if (e.keyCode === 27) {
+        _this.setState({ showVideoModal: false });
+      }
+    });
+  }
+
   render() {
-    console.log("slurp");
+
     return (
       <div className={ defaultStyles.pageContent }>
 
         <div className={ defaultStyles.homeContainer }>
           <SlidingImage url={'http://i.imgur.com/YrM9u1D.jpg'} />
         </div>
+
+        <p className={ defaultStyles.playVideoText + " pull-right" }
+           onClick={ this.toggleVideoModal }>Play video <FontAwesome className='super-crazy-colors' name='play-circle'/></p>
+
+        {/* Show the modal if the video modal button has been clicked */}
+        <VideoModal showModal={ this.state.showVideoModal } onChange={ this.modalChangeHandler }></VideoModal>
       </div>
     );
   }
---
+++ src/components/home/VideoModal.jsx
@@ -0,0 +1,30 @@
+import React, { PureComponent, PropTypes } from 'react';
+
+import defaultStyles from 'global.css';
+
+export default class VideoModal extends PureComponent {
+
+  static propTypes = {
+    showModal: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired
+  };
+
+  closeVideoModal = () => {
+    if (typeof this.props.onChange === 'function') {
+      this.props.onChange(false);
+    }
+  }
+  
+  render() {
+    if (this.props.showModal) {
+      return (
+          <div className={ defaultStyles.videoContainer }>
+            <span className={ defaultStyles.closeVideoModalButton } onClick={ this.closeVideoModal }>[ close ]</span>
+            <iframe className={ defaultStyles.vimeoVideo } src="https://player.vimeo.com/video/219104754?autoplay=0" frameBorder="0" allowFullScreen></iframe>
+          </div>
+      );
+    } else {
+      return null;
+    }
+  }
+}
